ca22de981a5d6234fdb9c07999083b416b1e3340
Vishal Vyas <vishal.v@zuru.tech>
2022-12-23 17:13:56 +0530

    terraform outputs

---
+++ terraform-gcp/outputs.tf
@@ -0,0 +1,27 @@
+output "region" {
+  value       = var.zone
+  description = "GCloud Region"
+}
+
+output "project_id" {
+  value       = var.project_id
+  description = "GCloud Project ID"
+}
+
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.primary.name
+  description = "GKE Cluster Name"
+}
+
+output "kubernetes_cluster_host" {
+  value       = google_container_cluster.primary.endpoint
+  description = "GKE Cluster Host"
+}
+
+output "gcp_static_ip" {
+  value       = google_compute_global_address.default.address
+  description = "GCP static IP"
+}
+
+
+
